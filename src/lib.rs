5cc8ef07df8c43a75501f734d70fe06df298bc81
CTHULHU-Jesus <endercreeper92@gmail.com>
2020-10-28 15:13:39 -0400

    Ready for a review/test.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::str::FromStr;
+use std::str::{FromStr};
 use std::string::ToString;
 use regex::Regex;
 use lazy_static::*;
@@ -13,8 +13,6 @@ pub enum Message {
     CHAT(String),
 }
 
-#[derive(Clone,PartialEq,Eq)]
-pub struct ParseMessageError(String);
 
 impl FromStr for Message {
     type Err = &'static str;
@@ -31,11 +29,19 @@ impl FromStr for Message {
         }
         match s {
             _ if HELLO.is_match(s) => Ok(Message::HELLO),
-            _ if NICK.is_match(s)  => Ok(Message::NICK("".to_string())),
+            _ if NICK.is_match(s)  => {
+                let mut mut_s = s.to_string();
+                mut_s.replace_range(..4,"");
+                Ok(Message::NICK(mut_s))
+            }
             _ if BYE.is_match(s)   => Ok(Message::BYE),
             _ if READY.is_match(s) => Ok(Message::READY),
             _ if RETRY.is_match(s) => Ok(Message::RETRY),
-            _ if CHAT.is_match(s)  => Ok(Message::CHAT("".to_string())),
+            _ if CHAT.is_match(s)  => {
+                let mut mut_s = s.to_string();
+                mut_s.replace_range(..4,"");
+                Ok(Message::CHAT(mut_s))
+            }
             _ => Err(ERR), 
 
         }
@@ -53,4 +59,33 @@ impl ToString for Message {
             Message::CHAT(m) => format!("CHAT{}",m),
         }
     }
+}
+
+/// tests to_sting and from_str for the Message type
+pub fn test_message() -> () {
+    // test HELLO,
+    println!("test HELLO to_string '{}'",Message::HELLO.to_string());
+    let hello_str : &str = "HELLO";
+    println!("test HELLO from_str '{}' -> '{}'", hello_str, Message::from_str(hello_str).unwrap().to_string());
+    // test NICK(String),
+    println!("test NICK to_string '{}'",Message::NICK("name".to_string()).to_string());
+    let nick_str : &str = "NICKname";
+    println!("test NICK from_str '{}' -> '{}'", nick_str, Message::from_str(nick_str).unwrap().to_string());
+    // test BYE,
+    println!("test BYE to_string '{}'",Message::BYE.to_string());
+    let bye_str : &str = "BYE";
+    println!("test BYE from_str '{}' -> '{}'", bye_str, Message::from_str(bye_str).unwrap().to_string());
+    // test READY,
+    println!("test READY to_string '{}'",Message::READY.to_string());
+    let ready_str : &str = "READY";
+    println!("test READY from_str '{}' -> '{}'", ready_str, Message::from_str(ready_str).unwrap().to_string());
+    // test RETRY,
+    println!("test RETRY to_string '{}'",Message::RETRY.to_string());
+    let retry_str : &str = "RETRY";
+    println!("test RETRY from_str '{}' -> '{}'", retry_str, Message::from_str(retry_str).unwrap().to_string());
+    // test CHAT(String),
+    println!("test CHAT to_string '{}'",Message::CHAT("words words".to_string()).to_string());
+    let chat_str : &str = "CHATwords words";
+    println!("test CHAT from_str '{}' -> '{}'", chat_str, Message::from_str(chat_str).unwrap().to_string());
+ 
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,46 @@ use std::net::*;
 use std::sync::*;
 use crate::lib::*;
 use std::io::{Read,Write};
+use std::time::Duration;
+
+/// Dissconnects from all active connections and the ends
+/// conns : The active connections
+fn disconnect_all_connections(conns : Vec<TcpStream>) {
+    const TIMEOUT_TIMER : Duration = Duration::from_secs(5);
+    // let mut conns : Vec<TcpStream>  = conns.clone();
+    println!("\nDisconnecting from all connections and closeing");
+    for conn in conns {
+        let conn = Box::new(conn);
+        std::thread::spawn( move || {
+            //@TODO tell client to shutdown
+            conn.shutdown(Shutdown::Both).unwrap();
+        });
+    };
+    std::thread::sleep(TIMEOUT_TIMER);
+}
+
+/// Removes a connection from a list of connections
+/// conns : The active connections
+/// to_remove : The connection to remove
+fn remove_connection(conns : &mut Vec<TcpStream>, to_remove : &TcpStream) {
+    let peer = to_remove.peer_addr().unwrap();
+    *conns = conns
+        .into_iter()
+        .filter(|x| x.peer_addr().unwrap() != peer)
+        .map(|x| x.try_clone().unwrap()).collect::<Vec<TcpStream>>();
+}
+
+/// Removes a nickname from the list of active nicknames
+/// nicknames : The list of active nicknames
+/// to_remove : The nickname to be removed
+fn remove_nickname(nicknames : &mut Vec<String>, to_remove : &String) {
+    *nicknames = nicknames
+        .into_iter()
+        .filter( |x| **x != *to_remove )
+        .map( |x| x.clone())
+        .collect();
+}
+
 
 /// Sends a message from one user to all other users
 /// conns : The current open connections.
@@ -10,17 +50,25 @@ use std::io::{Read,Write};
 /// nick  : The nickname of the user sending the message.
 /// message : The message being sent.
 fn blast_out(conns : &Vec<TcpStream>, me : &SocketAddr, nick : &String, message : &String) -> () {
-    //@TODO this
+    for mut connection in conns {
+        if connection.peer_addr().unwrap() != *me {
+            connection.write(
+                Message::CHAT(format!("{}:{}",nick,message))
+                .to_string()
+                .as_bytes()
+            ).unwrap();
+        }
+    }
 }
 
 /// Gets a valid nickname from the user 
 /// and adds the nickname to the list of nicknames in use
 /// stream : the tcpstream connected to the user
 /// nicknames : the global list of all nicknames
-/// returns : a valid nickname
-fn getNickname(stream : &TcpStream, nicknames : Arc<Mutex<Vec<String>>>) -> String {
-    let mut message = [0;1024];
-    let nick : String;
+/// returns : a valid nickname or None if the user wants to end the connection
+fn get_nickname(stream : &mut TcpStream, nicknames : &Arc<Mutex<Vec<String>>>) -> Option<String> {
+    const NAME_MAX_SIZE : usize = 32;
+    let mut message = [0;NAME_MAX_SIZE];
     while match stream.read(&mut message) {
         Ok(size) => {
             let message : Message =
@@ -30,74 +78,105 @@ fn getNickname(stream : &TcpStream, nicknames : Arc<Mutex<Vec<String>>>) -> Stri
                 )
                 .unwrap();
             match message {
-                Message::NICK(n) => true, //@TODO FINISH THIS
+                Message::NICK(n) => {
+                    // if the nickname is not taken set add it to the list of nicknames in
+                    // use and then return the nick
+                    if !nicknames.lock().unwrap().contains(&n.clone()) {
+                        nicknames.lock().unwrap().push(n.clone());
+                        stream.write(Message::READY.to_string().as_bytes()).unwrap();
+                        return Some(n);
+                    } else {
+                        // else ask the client to retry
+                        stream.write(Message::RETRY.to_string().as_bytes()).unwrap();
+                        true
+                    }
+                }
+                Message::BYE => return None,
                 _ => true,
             }
         },
         Err(_) => true
-    } {};
-    nick;
+    } {
+        message = [0;NAME_MAX_SIZE];
+    };
+    None
 }
 
 
 fn main() {
     // start listening for connections
-    // @TODO change portnumber to come from a command line argument
-    let portnumber = 1337; //1337,8008,42069
-    let listener = TcpListener::bind(format!("0.0.0.0:{}",portnumber)).unwrap();
+    const USAGE : &str = "cargo run [port number]";
+    const MESSAGE_MAX_SIZE : usize = 4000;
+    let portnumber = std::env::args().nth(1).expect(USAGE).parse::<u32>().expect(USAGE); //1337,8008,42069
+    let listener = TcpListener::bind(format!("0.0.0.0:{}",portnumber)).expect("Could not bind to desired port number");
     let connections : Arc<Mutex<Vec<TcpStream>>> = Arc::new(Mutex::new(Vec::new()));
     let nicknames : Arc<Mutex<Vec<String>>>   = Arc::new(Mutex::new(Vec::new()));
+    // set up signal handler for ^C
+    {
+        let connections = connections.clone();
+        ctrlc::set_handler( move || {
+            disconnect_all_connections(connections.lock().unwrap().iter().map( |x| x.try_clone().unwrap() ).collect());
+            std::process::exit(0);
+        }).expect("Error seting Ctrl-C handler");
+    }
     for stream in listener.incoming() {
         let stream = Box::new(stream);
         let connections = connections.clone();
-        let mut message : [u8;1024] = [0;1024];
-        // spin up thread to handel each client
+        let nicknames = nicknames.clone();
+        // spin up thread to handle each client
         std::thread::spawn( move || {
             //Start
             let mut stream = (*stream).unwrap();
             (*connections.lock().unwrap()).push(stream.try_clone().unwrap());
+
             // Send HELLO Message 
             stream.write(&Message::HELLO.to_string().as_bytes()).unwrap();
+
             // Handel Nick Message
-            let nick : String = getNickname(&stream,nicknames);
+            let nick : String = match get_nickname(&mut stream,&nicknames) {
+                Some(s) => s,
+                None    => {
+                    // None means that the client wants to disconnect
+                    remove_connection(&mut connections.lock().unwrap(),&stream);
+                    stream.shutdown(Shutdown::Both).expect("Could not shutdown connection");
+                    std::process::exit(0);
+                }
+            };
 
             // Wait for messages
+            let mut message = [0u8;MESSAGE_MAX_SIZE];
             while match stream.read(&mut message) {
                 Ok(size) => {
                     let message : Message =
                         std::str::FromStr::from_str(
-                        std::str::from_utf8(
-                            &message[0..size]).unwrap()
-                        )
+                        std::str::from_utf8(&message[0..size])
+                        .unwrap())
                         .unwrap();
                     match message {
+                        // On CHAT blast it out to all connected users
                         Message::CHAT(x) => {
-                            blast_out(&connections.lock().unwrap(),&stream.peer_addr().unwrap(),&nick);
+                            blast_out(&connections.lock().unwrap(),&stream.peer_addr().unwrap(),&nick,&x);
                             true
                         }
+                        // on BYE exit loop
                         Message::BYE => false,
+                        // Do not process any other messages, but do loop back
                         _ => true,
                     }
                 },
+                // On Error do nothing but loop back
                 Err(_) => true,
             } {
                 // clears buffer
-                message = [0;1024];
+                message = [0;MESSAGE_MAX_SIZE];
             };
 
             //End
             //remove nickname from list of nicknames in use nicknames
-            *nicknames.lock().unwrap() = nicknames.lock().unwrap()
-                .iter()
-                .filter( |x| **x != nick )
-                .map( |x| *x )
-                .collect();
+            remove_nickname(&mut nicknames.lock().unwrap(),&nick);
             //remove connection from list of connections in use
-            let peer = stream.peer_addr().unwrap();
-            *connections.lock().unwrap() = (*connections.lock().unwrap())
-                .iter()
-                .filter(|x| x.peer_addr().unwrap() != peer)
-                .map(|x| x.try_clone().unwrap()).collect();
+            remove_connection(&mut connections.lock().unwrap(),&stream);
+            stream.shutdown(Shutdown::Both).expect("Could not shutdown connection");
         });
     }
 }
